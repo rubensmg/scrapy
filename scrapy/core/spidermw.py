91c36d60c8744a3a0e693cada3b6180a7ca05530
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2018-06-22 22:26:35 -0300

    [WIP] Just trying something, not quite there yet

--- scrapy/core/spidermw.py
+++ scrapy/core/spidermw.py
@@ -3,6 +3,8 @@ Spider Middleware manager
 
 See documentation in docs/topics/spider-middleware.rst
 """
+from functools import wraps
+
 import six
 from twisted.python.failure import Failure
 from scrapy.exceptions import _InvalidOutput
@@ -10,12 +12,16 @@ from scrapy.middleware import MiddlewareManager
 from scrapy.utils.defer import mustbe_deferred
 from scrapy.utils.conf import build_component_list
 
+
 def _isiterable(possible_iterator):
     return hasattr(possible_iterator, '__iter__')
+
 
 class SpiderMiddlewareManager(MiddlewareManager):
 
     component_name = 'spider middleware'
+
+    mw_methods = []  # list of dicts (process_spider_output, process_spider_exception)
 
     @classmethod
     def _get_mwlist_from_settings(cls, settings):
@@ -25,12 +31,13 @@ class SpiderMiddlewareManager(MiddlewareManager):
         super(SpiderMiddlewareManager, self)._add_middleware(mw)
         if hasattr(mw, 'process_spider_input'):
             self.methods['process_spider_input'].append(mw.process_spider_input)
-        if hasattr(mw, 'process_spider_output'):
-            self.methods['process_spider_output'].insert(0, mw.process_spider_output)
-        if hasattr(mw, 'process_spider_exception'):
-            self.methods['process_spider_exception'].insert(0, mw.process_spider_exception)
         if hasattr(mw, 'process_start_requests'):
             self.methods['process_start_requests'].insert(0, mw.process_start_requests)
+
+        self.mw_methods.insert(0, dict(
+            process_spider_output=getattr(mw, 'process_spider_output', None),
+            process_spider_exception=getattr(mw, 'process_spider_exception', None),
+        ))
 
     def scrape_response(self, scrape_func, response, request, spider):
         fname = lambda f:'%s.%s' % (
@@ -48,45 +55,52 @@ class SpiderMiddlewareManager(MiddlewareManager):
                     return scrape_func(Failure(), request, spider)
             return scrape_func(response, request, spider)
 
-        def process_spider_exception(_failure):
-            exception = _failure.value
-            # don't handle _InvalidOutput exception
-            if isinstance(exception, _InvalidOutput):
-                return _failure
-            for method in self.methods['process_spider_exception']:
+        def _wrapper_process_spider_output(method):
+            @wraps(method)
+            def callback(result):
+                result = method(response=response, result=result, spider=spider)
+                if _isiterable(result):
+                    for elem in result:
+                        yield elem
+                else:
+                    raise _InvalidOutput('Middleware {} must return an iterable object, ' \
+                        'got {}'.format(fname(method), type(result)))
+            return callback
+
+        def _wrapper_process_spider_exception(method):
+            @wraps(method)
+            def errback(failure):
+                exception = failure.value
+                # don't handle _InvalidOutput exception
+                if isinstance(exception, _InvalidOutput):
+                    return failure
                 result = method(response=response, exception=exception, spider=spider)
                 if result is not None and not _isiterable(result):
                     raise _InvalidOutput('Middleware {} must return None or an iterable ' \
                         'object, got {}'.format(fname(method), type(result)))
-                # stop exception handling by handing control over to the
-                # process_spider_output chain if an iterable has been returned
-                if result is not None:
-                    return result
-            return _failure
-
-        def process_spider_output(result):
-            def wrapper(result_iterable):
-                try:
-                    for r in result_iterable:
-                        yield r
-                except Exception as ex:
-                    exception_result = process_spider_exception(Failure(ex))
-                    if exception_result is None or isinstance(exception_result, Failure):
-                        raise
-                    for output in exception_result:
-                        yield output
-            for method in self.methods['process_spider_output']:
-                result = method(response=response, result=result, spider=spider)
-                if _isiterable(result):
-                    result = wrapper(result)
-                else:
-                    raise _InvalidOutput('Middleware {} must return an iterable object, ' \
-                        'got {}'.format(fname(method), type(result)))
-            return result
+                return failure if result is None else result
+            return errback
 
         dfd = mustbe_deferred(process_spider_input, response)
-        dfd.addErrback(process_spider_exception)
-        dfd.addCallback(process_spider_output)
+        dfd.pause()
+        for mw in self.mw_methods:
+            if all(mw.values()):
+                dfd.addCallbacks(
+                    callback=_wrapper_process_spider_output(mw['process_spider_output']),
+                    errback=_wrapper_process_spider_exception(mw['process_spider_exception']))
+            elif mw['process_spider_output']:
+                dfd.addCallback(_wrapper_process_spider_output(mw['process_spider_output']))
+            elif mw['process_spider_exception']:
+                dfd.addErrback(_wrapper_process_spider_exception(mw['process_spider_exception']))
+        dfd.unpause()
+        # XXX - debug
+        print('='*100)
+        for _m in dfd.callbacks:
+            print(_m)
+        print('='*100)
+        # import pdb
+        # pdb.set_trace()
+        # XXX - debug
         return dfd
 
     def process_start_requests(self, start_requests, spider):
